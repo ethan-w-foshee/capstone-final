8f79be81d61eccafc2d48f611f2db668249bfdd9
ethan.w.foshee <skyhailer2@gmail.com>
2023-05-10 19:27:43 -0500

    Basic setup for backend

--- src/sql/controllers/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const usersController = require('../controllers/users')
-
-router.get('/', usersController.list)
-
-module.exports = router
